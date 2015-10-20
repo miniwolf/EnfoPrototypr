5971441017d2304dd6ae0fcf502167264867243e
Vangunderfanderson <juanmajxk@gmail.com>
2015-10-20 14:35:27 +0200

    Fixed UI for 16:9

---
+++ Assets/Scripts/UI/MinimapClickPositioningScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapClickPositioningScript : MonoBehaviour {
+
+	private RaycastHit hit;
+	private Ray ray;
+	private Camera minimapCamera;
+
+	void Awake(){
+		minimapCamera = gameObject.GetComponent<Camera>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			if(Physics.Raycast(ray, out this.hit, Mathf.Infinity, LayerMask.GetMask("Ground"))){
+
+			}
+		}
+	}
+}
