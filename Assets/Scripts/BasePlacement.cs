a90427d30ce4647f95ce4aaa1ccbbf009d283cef
Sindri <sindriwest@gmail.com>
2015-10-25 10:32:52 +0100

    Base Placement script added to Terrain object

---
+++ Assets/Scripts/BasePlacement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlacement : MonoBehaviour {
+
+    public GameObject homebase;
+
+    // Default base position and rotation
+    public bool setDefault = true;
+
+    // Position and rotation parameters
+    public float xCoord, yCoord, zCoord;
+    public float xRot, yRot, zRot;
+
+    void Start () {
+        Invoke("SpawnBase", 0);
+    }
+
+    void SpawnBase()
+    {
+        if (setDefault)
+        {
+            Instantiate(homebase, homebase.transform.position, homebase.transform.rotation);
+        }
+        else
+        {
+            Vector3 pos = new Vector3(xCoord, yCoord, zCoord);
+            Quaternion rot = Quaternion.Euler(xRot, yRot, zRot);
+            Instantiate(homebase, pos, rot);
+        }
+    }
+}
