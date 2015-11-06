59310af423d77d8d79a4d39d500d310999e3d459
Sindri <sindriwest@gmail.com>
2015-11-06 13:13:28 +0100

    OSC changes

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class BackgroundMusic : MonoBehaviour {
 
-    private float seconds = 180.0f;
+    private float musicRepeatSecs = 180.0f;
+    private float windRepeatSecs = 10.0f;
+
 
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("musicOSC", 0, seconds);
+        InvokeRepeating("musicOSC", 0, musicRepeatSecs);
+        InvokeRepeating("windOSC", 0, windRepeatSecs);
     }
 
     // Update is called once per frame
@@ -19,5 +22,11 @@ public class BackgroundMusic : MonoBehaviour {
     {
         float music = 1.0f;
         OSCHandler.Instance.SendMessageToClient("SuperCollider", "/music", music);
+    }
+
+    void windOSC()
+    {
+        float wind = 1.0f;
+        OSCHandler.Instance.SendMessageToClient("SuperCollider", "/wind", wind);
     }
 }
--- Assets/Scripts/navScript.cs
+++ Assets/Scripts/navScript.cs
@@ -4,21 +4,13 @@ using System.Collections;
 public class navScript : MonoBehaviour {
 
     public GameObject target;
-    float seconds = 0.75f;
 
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("moveOSC", 0, seconds);
     }
 
     // Update is called once per frame
     void Update () {
         gameObject.GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
-    }
-
-    void moveOSC()
-    {
-        float move = 1.0f;
-        OSCHandler.Instance.SendMessageToClient("SuperCollider", "/move", move);
     }
 }
