64643d5f840a967f07ab8e1c1f8d9ab36ebf8314
Vangunderfanderson <juanmajxk@gmail.com>
2015-11-17 14:10:14 +0100

    minor changes to the buying

--- Assets/Scripts/UI/ActionButtonScript.cs
+++ Assets/Scripts/UI/ActionButtonScript.cs
@@ -11,23 +11,36 @@ public class ActionButtonScript : MonoBehaviour {
 	public void setDescription(string desc){
 		description = desc;
 	}
+	public string getDescription(){
+		return description;
+	}
 	public void setPrice(int price){
 		this.price = price;
 	}
-
-	public void setIsInShopButton(bool value){
+	public int getPrice(){
+		return price;
+	}
+	public void setIsInShop(bool value){
 		isInShop = value;
+	}
+	public bool getIsInShop(){
+		return isInShop;
 	}
 	public void showDescription(){
 		UIManager.showDescription(description);
 	}
-
 	public void stopShowDescription(){
 		UIManager.stopShowDescription();
 	}
 	public void clickAction(){
 		if(isInShop){
-			UIManager.addButtonToInventory(Instantiate(this.gameObject),price);
+			GameObject newButton = Instantiate(this.gameObject);
+			ActionButtonScript actionButtonScript = newButton.GetComponent<ActionButtonScript>();
+			actionButtonScript.setDescription(description);
+			actionButtonScript.setPrice(price);
+			actionButtonScript.setIsInShop(isInShop);
+
+			UIManager.addButtonToInventory(newButton,price);
 		}else{
 			//do action
 		}
