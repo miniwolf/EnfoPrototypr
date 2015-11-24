488a2079f68dd930783745261f7ba3a43721b0b9
Sindri <sindriwest@gmail.com>
2015-11-24 14:50:19 +0100

    ours TagManager.asset used

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class GameOverManager : MonoBehaviour {
 
+    //public Transform target;
+    //public GameObject enemy;
+    private bool gameOver = false;
+    public static int targetLife = 20;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        if(targetLife <= 0)
+        {
+            gameOver = true;
+        }
+        if(gameOver && (Application.loadedLevel != 0 || Application.loadedLevel != 1))
+        {
+            Application.LoadLevel("gameOver");
+        }
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.gameObject.tag == "Enemy")
+        {
+            targetLife -= 1;
+            Destroy(col.collider.gameObject);
+        }
+    }
 }
